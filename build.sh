6ff5373753379aa439fb0beb596f0003a7641046
Andrei Zeucianu <andrei.zeucianu@gmail.com>
2024-10-28 21:58:54 +0100

    Added option to compile with gcc in the build system
    Also notified this change in the README.md file.

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@ set -xe
 
 mkdir -p build/
 
+COMPILER="clang" # or gcc (both works)
 CFLAGS="-Wall -Wextra"
 CINCLUDE="-I./raylib/raylib-5.0/src"
 CLINK="-L./raylib/raylib-5.0/src/ -lraylib -lGL -lm -lpthread -ldl -lrt -lX11"
 DEBUG="-g"
 
-clang $DEBUG $CFLAGS -o build/raypong src/*.c $CINCLUDE $CLINK
+$COMPILER $DEBUG $CFLAGS -o build/raypong src/*.c $CINCLUDE $CLINK
