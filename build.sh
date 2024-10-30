8eb4e315d9650432488033ff6476d84d9d69cc8f
Andrei Zeucianu <andrei.zeucianu@gmail.com>
2024-10-30 12:24:07 +0100

    Updated the build system again
    The build system will now build only what is necessary. It will build
    Raylib only if necessary and if now creates symlink of the library in
    the build folder instead of copying the library.

--- build.sh
+++ build.sh
@@ -2,17 +2,27 @@
 
 set -xe
 
-# Compiling Raylib
-cd raylib/raylib-5.0/src
-make PLATFORM=PLATFORM_DESKTOP
-cd -
-
-# Copy raylib library in build folder
-cp ./raylib/raylib-5.0/src/raylib.h ./build/
-cp ./raylib/raylib-5.0/src/libraylib.a ./build/
-
 # Create the build folder
 mkdir -p build/
+
+if ! test -e ./raylib/raylib-5.0/src/libraylib.a
+then
+    # Compiling Raylib
+    cd raylib/raylib-5.0/src
+    make PLATFORM=PLATFORM_DESKTOP
+    cd -
+fi
+
+# Create raylib symlink in build folder
+if ! test -e ./build/raylib.h
+then
+    ln -s ../raylib/raylib-5.0/src/raylib.h build/
+fi
+
+if ! test -e ./build/libraylib.a 
+then
+    ln -s ../raylib/raylib-5.0/src/libraylib.a build/
+fi
 
 # Compiler options
 COMPILER="clang" # or gcc (both works)
